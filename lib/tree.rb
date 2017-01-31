a9ab34f8688681a9a399ac445dcd444f94ed257e
melvinthemok <melvinmok.cy@gmail.com>
2017-01-31 16:17:04 +0800

    Complete exercise

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,7 +1,14 @@
 class Tree
+  attr_reader :type
+  attr_accessor :height, :age
+
   def initialize(type, height, age)
+    @type = type
+    @height = height
+    @age = age
   end
 
   def grow_tree(water_amount)
+    self.height += water_amount / 10
   end
 end
